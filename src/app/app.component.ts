39047ea787247fc80688bdd0a0c9ef96b1854927
pberlanda <paolo.berlanda@marconirovereto.it>
2022-03-14 20:55:05 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,4 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
   gender = 'F';
-  /*clickHandler() {
-    console.log('click');
-  }
-  inputHandler() {
-    console.log('press');
-  }*/
-  
 }
